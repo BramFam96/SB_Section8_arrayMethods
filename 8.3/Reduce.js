698dd672cbf0739230b85691d50a3b33a80bf84c
BramFam96 <mtbramlett25@gmail.com>
2021-11-02 22:41:29 -0500

    Added reducer exercises

---
+++ 8.3/Reduce.js
@@ -0,0 +1,38 @@
+//Reduce is troublesome because of its broad versatility and use-cases;
+//It can extract data, create a new datastructures, or any other
+//operation that reduce a dataset to a single value;
+
+const nums = [20, 30, 50, 12, -2, 45, 99, 19, 22, 85]
+
+//find sum
+let tot = 0
+for (let num of nums) {
+	total += num
+}
+console.log(total)
+
+//find min
+let min = nums[0]
+for (let i = 1; i < num.length; i++) {
+	if (nums[i] < min) min = nums[i]
+}
+console.log(`min is ${min}`)
+
+const str = 'lollapalooza'
+const charFreq = {}
+for (let char of str) {
+	if (charFreq[char]) charFreq[char] += 1
+	else charFreq[char] = 1
+}
+console.log(charFreq)
+//Reduce iterates through an arr and takes a callback like our other methods
+//main diff is reduce's callback accepts an accumulator and a name for our individual value
+//Reduce will update our accumulator when the callback passes'
+//ie we can reduce for sum, or median
+//we can also reduce into an object -> ie count of letters in a string
+
+//find min
+
+Array.reduce((min, nextVal) => {
+	return nextVal < min ? nextVal : min
+}, 'Optional accumulator starting value')
---
+++ ReduceExercise.js
@@ -0,0 +1,42 @@
+//1) Extract values from obj
+
+const extractValues = (userArr, name) => {
+	return userArr.reduce((newArr, nextUser) => {
+		newArr.push(nextUser[name])
+		return newArr
+	}, [])
+}
+
+//2) vowelCount
+const vowelCount = (str) => {
+	const vowelStr = 'aeiou'
+	const strArr = str.split('')
+	return strArr.reduce((vowelCount, nextChar) => {
+		if (vowelStr.indexOf(nextChar.toLowerCase()) !== -1) {
+			vowelCount[nextChar.toLowerCase()]++
+		} else {
+			vowelCount[nextChar.toLowerCase()] = 1
+		}
+		return vowelCount
+	}, {})
+}
+//3) addKeyValuePair
+
+const addKeyAndValue = (arrOfObjs, key, value) => {
+	return arrOfObjs.reduce((newArrOfObjs, nextObj, currentIndex) => {
+		newArrOfObjs[currentIndex][key] = value
+		return newArrOfObjs
+	}, arrOfObjs)
+}
+//4) partition into two arrays
+
+const partition = (arr, callback) => {
+	return arr.reduce(
+		(newArrOfArrs, nextVal) => {
+			callback(nextVal)
+				? newArrOfArrs[0].push(nextVal)
+				: newArrOfArrs[1].push(nextVal)
+		},
+		[[], []]
+	)
+}
